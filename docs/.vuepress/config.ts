1d3a221d81765c3d43b91017377f644dc85e8b74
weiquanju <anbine@qq.com>
2022-04-15 01:12:30 +0800

    feat: 新增配置、demoBlock插件、导航

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,17 +1,68 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
-
+import path from 'path'
+import demoBlock from 'vuepress2-plugin-demo-block'
 export default defineUserConfig<DefaultThemeOptions>({
-  // 站点配置
-  lang: 'zh-CN',
-  title: 'V-El-Table',
-  description: '一个基于Element-Plus，富有表现力的表格组件。打造你的的生产力！',
 
   // 主题和它的配置
   theme: '@vuepress/theme-default',
   themeConfig: {
     logo: '/assets/images/logo.png',
+    repo: 'weiquanju/v-el-table',
+    navbar: [
+      // 嵌套 Group - 最大深度为 2
+      {
+        text: '文档',
+        link: '/guide',
+      },
+      {
+        text: '组件',
+        link: '/component',
+      },
+      // {
+      //   text: '文档',
+      //   children: [
+      //     {
+      //       text: 'Always active',
+      //       link: '/',
+      //       // 该元素将一直处于激活状态
+      //       activeMatch: '/',
+      //     },
+      //     {
+      //       text: 'Active on /foo/',
+      //       link: '/not-foo/',
+      //       // 该元素在当前路由路径是 /foo/ 开头时激活
+      //       // 支持正则表达式
+      //       activeMatch: '^/foo/',
+      //     },
+      //   ],
+      // },
+    ],
+    locales: {
+      '/': {
+        selectLanguageName: '简体中文',
+      },
+      '/en/': {
+        selectLanguageName: 'English',
+      },
+    },
   },
+
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN',
+      title: 'V-El-Table',
+      description: '一个基于Element-Plus，富有表现力的表格组件。打造你的的生产力！',
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'V-El-Table',
+      description: 'An expressive form component based on element plus. Build your productivity!',
+    },
+  },
+
   head: [
     [
       'link',
@@ -34,4 +85,10 @@ export default defineUserConfig<DefaultThemeOptions>({
     ['meta', { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
     ['link', { rel: "apple-touch-icon", href: "/assets/images/icons/apple-app.png" }],
   ],
+  plugins: [[
+    demoBlock,
+    {
+      componentsDir: path.resolve(__dirname, './../examples')
+    }
+  ]],
 })
