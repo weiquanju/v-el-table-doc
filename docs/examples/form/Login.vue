acc354705642b983447ba9fc8aa45dfbad146d79
weiquanju <anbine@qq.com>
2022-05-06 15:28:53 +0800

    fix: 拆分示例

--- docs/examples/form/Login.vue
+++ docs/examples/form/Login.vue
@@ -2,19 +2,12 @@
     <VElForm ref="form" v-bind="config" />
 </template>
 <script lang="tsx" setup>
-import { ElOption } from 'element-plus';
 import { VElForm } from 'v-el-table'
-import { reactive, ref, h, SetupContext } from 'vue';
-
-function MyInputString(props: { modelValue: string; type: string; placeholder: string }, ctx: SetupContext) {
-    const { type, placeholder } = props
-    return h('input', { type, placeholder, value: props.modelValue, onInput: (e: any) => ctx.emit('update:modelValue', e.target.value) })
-}
-const name = ref()
+import { reactive, ref } from 'vue';
 
 const form = ref<any>(null)
 
-const model = ref({ username: 'Han Meimei', num: 1, app: [], remember: true, anonymousName: '' })
+const model = ref({ username: 'Han Meimei', password: '', remember: true })
 const config = reactive({
     form: {
         model: model,
@@ -34,26 +27,6 @@ const config = reactive({
             itemProps: { prop: 'password', label: '密码', rules: [{ required: true, message: '请输入密码' }] },
             inputComponent: 'ElInput',
             inputProps: { type: 'password', placeholder: '请输入密码', showPassword: true },
-            inputEvents: {
-                change: (...args: any) => console.log(...args)
-            }
-        },
-        {
-            itemProps: { prop: 'app', label: '应用', rules: [{ required: true, message: '请选择应用' }] },
-            inputComponent: 'el-select',
-            inputProps: { type: 'password', placeholder: '请选择应用' },
-            inputEvents: {
-                change: (...args: any) => console.log(...args)
-            },
-            // select options
-            children: [{ name: '游戏APP', id: 1 }, { name: '学习APP', id: 2 },].map(({ name, id }) => {
-                return <ElOption label={name} value={id} />
-            })
-        },
-        {
-            itemProps: { prop: 'anonymousName', label: '马甲', rules: [{ required: true, message: '请输入马甲' }] },
-            inputComponent: MyInputString,
-            inputProps: { type: 'text', placeholder: '请输入马甲' },
             inputEvents: {
                 change: (...args: any) => console.log(...args)
             }
